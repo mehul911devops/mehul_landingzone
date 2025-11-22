1b8fd6f9862b9ec2158617e7f23dc89215db62b5
mehul911devops <mehul911@gmail.com>
2025-11-22 17:01:13 +0530

    v3

--- env/dev/main.tf
+++ env/dev/main.tf
@@ -4,11 +4,13 @@ module "mrg" {
 }
 
 module "mstoragem" {
+    depends_on = [ module.mrg ]
     source = "../../modules/STG"
     storagem = var.storagem
 }
 
 module "mnetwork" {
+    depends_on = [ module.mrg ]
     source = "../../modules/NETWORKING"
     networkm = var.networkm
 }
